41da707a875c6fe69f3e3ffcc12a776dfcf8de97
re004037 <re004037@yahoo.co.jp>
2017-07-15 06:09:00 +0000

    問題１の答え

--- anly_1.py
+++ anly_1.py
@@ -1,19 +1,24 @@
-# 1分足を5分足にまとめる。
 #coding:utf-8連続ではない
+# 1分足を5分足にまとめる。
+
 import csv   #csvモジュールをインポートする
 import os
 
 header = ['date','O','H','L','C']
 
-# 
-r = open('', 'rb')
+# 入力ファイルの読込
+r = open('INPUT/AUDJPY_20150501.csv', 'rb')
+
+# 配列に格納
 dataReader = csv.reader(r)
 header = next(dataReader)
 
-w = open('INPUT/','wb')
+# 出力ファイルの設定
+w = open('OUTPUT/AUDJPY_20150501.csv','wb')
 writer = csv.writer(w, lineterminator='\n')
 writer.writerow(header)
+for row in dataReader:
+    writer.writerow(row)
 
+r.close()
 w.close()
-
-    
